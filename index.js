d2a4520245c9fd3752b5a6fac47938f5917a3fca
Randy Herasme <flatironschool@Flatirons-MacBook-Air.local>
2018-11-21 12:30:54 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,52 @@ const questions = [
 ]
 
 let question;
+
+
+function trueAndFalseButtons(){
+  return btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
+}
+
+function appendQuestion(question){
+  let container = document.querySelector('.question-container')
+  container.innerHTML = question.questionText;
+}
+
+function removeQuestion(){
+  return new Promise(function(){
+    let container = document.querySelector('.question-container')
+    container.innerHTML = ''
+    toggleTrueAndFalseButtons()
+  })
+}
+
+//
+
+
+function askQuestionThen(time){
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise(function(resolve){
+    setTimeout(function(){
+      resolve(question)
+    }, time)
+  })
+}
+
+function askQuestionThenRemoveQuestion(time){
+  return askQuestionThen(time).then(removeQuestion)
+}
+
+function toggleTrueAndFalseButtons(){
+  trueAndFalseButtons().forEach(function(btn){
+    btn.classList.toggle("hide")
+  })
+}
+
+function displayQuestionOnClick(){
+  let btn = document.querySelector('a')
+  return btn.addEventListener('click', () => {
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
