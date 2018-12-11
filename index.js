7d5968b94c2fc556f438bb9aaba43a2051c96624
Mendy <mendy.cohen@flatironschool.com>
2018-12-11 10:39:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+// const questions = [
+//   {questionText: "Lightning never strikes in the same place twice", answer: false},
+//   {questionText: "Humans can distinguish between over one trillion different smells", answer: true},
+//   {questionText: "Goldfish only have a memory of about three seconds", answer: false}
+// ]
+//
+// const btn = document.querySelector('.waves-effect.waves-light.btn')
+//  btn.addEventListener('click', displayQuestionOnClick)
+//
+// let question;
+//
+// const questionContainer = document.querySelector('.question-container')
+//
+// function appendQuestion(){
+//   questions.forEach(question => {
+//     questionContainer.innerHTML += `<li>${question.questionText}</li>`
+//   })
+// }
+//
+// function askQuestionThen(time){
+//    setTimeout(function(){  }, time);
+//   question = questions[0].questionText
+// }
+//
+// function removeQuestion(time){
+//   setTimeout(function(){ questionContainer.innerHTML = "" }, time);
+// }
+//
+// function askQuestionThenRemoveQuestion(time){
+//   appendQuestion()
+//   removeQuestion(time)
+// }
+//
+// function trueAndFalseButtons(){
+//   return btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
+// }
+//
+// function toggleTrueAndFalseButtons(){
+//   trueAndFalseButtons().forEach(function(btn){
+//     btn.classList.toggle("hide")
+//
+//   })
+// }
+//
+// function displayQuestionOnClick(){
+//   toggleTrueAndFalseButtons()
+//   askQuestionThenRemoveQuestion(5000).then(questionContainer.innerHTML = "")
+  // questionContainer.innerHTML = ""
+//}
+
+
 const questions = [
   {questionText: "Lightning never strikes in the same place twice", answer: false},
   {questionText: "Humans can distinguish between over one trillion different smells", answer: true},
@@ -5,3 +56,55 @@ const questions = [
 ]
 
 let question;
+function askQuestion(){
+  return questions[0]
+}
+
+function displayQuestionOnClick(){
+  let btn = document.querySelector('a')
+  return btn.addEventListener('click', () => {
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
+
+function trueAndFalseButtons(){
+  return btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
+}
+
+function toggleTrueAndFalseButtons(){
+  trueAndFalseButtons().forEach(function(btn){
+    btn.classList.toggle("hide")
+  })
+}
+
+function checkQuestion(question, answer){
+  question.questionAnswer == answer
+}
+
+function askQuestionThen(time){
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise(function(resolve){
+    setTimeout(function(){
+      resolve(question)
+    }, time)
+  })
+}
+
+function appendQuestion(question){
+  let container = document.querySelector('.question-container')
+  container.innerHTML = question.questionText;
+}
+
+function removeQuestion(){
+  return new Promise(function(){
+    let container = document.querySelector('.question-container')
+    container.innerHTML = ''
+    toggleTrueAndFalseButtons()
+  })
+}
+
+function askQuestionThenRemoveQuestion(time){
+  return askQuestionThen(time).then(removeQuestion)
+}
